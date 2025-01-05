8bbf47881484574ba6289dc7b4659f599b6feeed
5cm/s <you@example.com>
2025-01-05 06:55:34 -0800

    s

--- ConsoleApplication1/ConsoleApplication1/Cpp.cpp
+++ ConsoleApplication1/ConsoleApplication1/Cpp.cpp
@@ -1,16 +1,19 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++V2+++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #include <iostream>
 #include <string>
 
-
+using namespace std;
 
 //+++++++++++++++++++++++++++++++++++++++++++++++Include Lua headers++++++++++++++++++++++++++++++++++++++++
 extern "C"
 {
-#include  "Lua/include/lua.h"
+#include "Lua/include/lua.h"
 #include "Lua/include/lauxlib.h"
 #include "Lua/include/lualib.h"
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++Link Lua library for Windows+++++++++++++++++++++++++
 #ifdef _WIN32
@@ -19,35 +22,39 @@ extern "C"
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-using namespace std;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++Check for error msg++++++++++++++++++++++++++++++++++
+bool Checklua(lua_State* L, int r) {
+
+	if (r != LUA_OK) {
+		string errormsg = lua_tostring(L, -1);
+		cout << errormsg << endl;
+		return false;
+	}
+	return true;
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 int main() {
 
-	string cmd = "a = 7 + 165";
+	
 
 	//lua code compiller
 	lua_State* L = luaL_newstate();
 
-	int r = luaL_dostring(L, cmd.c_str());
+	//lua libs for handle math usage
+	luaL_openlibs(L);
 
+	if (Checklua(L, luaL_dofile(L, "Luascript.lua"))) {
 
-	//validation
-	if (r == LUA_OK) {
-
-		//extract value a as number so we can display it
 		lua_getglobal(L, "a");
-		if (lua_isnumber(L, -1))
-		{
-			float a_in_cpp = (float)lua_tonumber(L, -1);
-			cout << "a_in_cpp = " << a_in_cpp << endl;
+		if (lua_isnumber(L, -1)) {
+			float result = (float)lua_tonumber(L, -1);
+			cout << "result: " << result << endl;
 		}
-
-	}else
-	{
-		string errormsg = lua_tostring(L, -1);
-		cout << errormsg << endl;
-
 	}
+	
+
 
 	//stop lua virtual machine
 	lua_close(L);
